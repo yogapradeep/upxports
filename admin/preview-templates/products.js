c1676522a941a1023941c3ba7a63f62b648d717b
pradeep <v.yogapradeep@gmail.com>
2019-10-14 22:18:36 +0530

    remove modal content

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -13,40 +13,7 @@ const page = createClass({
 
     
   <section id="products" class="section is-medium has-css_bg-color">
-  ${entry.getIn(["data", "products"], []).map(pro =>   html`
-    <div id="${pro.get("title")}" class="modal">
-      <div class="modal-background has-fadein-animation"></div>
-      <div class="modal-card has-fadein-animation">
-        <header class="modal-card-head">
-          <p class="modal-card-title">${pro.get("title")}</p>
-          <button class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="content">
-            <figure class="image is-4by3 is-hidden-desktop has-no-margin">
-              <img class="lazy" data-src="${getAsset(pro.get("image1"))}" alt="${pro.get("title")}" loading="lazy" />
-            </figure>
-            <figure class="image is-16by9 is-hidden-touch has-no-margin">
-              <img class="lazy" data-src="${getAsset(pro.get("image2"))}" alt="${pro.get("title")}" loading="lazy" />
-            </figure>
-            <p>
-            ${pro.get("text")}
-            </p>
-            <h2 class="has-margin-bottom-2">${pro.get("category")}</h2>
-          
-           
-           <blockquote>
-           ${pro.get("flagship")}
-            </blockquote>   
-           
-        </section>
-      </div>
-    </div>
-   
-  `)} 
-   
-
-
+ 
   <div class="container">
   <header class="has-text-centered">
     <h3 class="title is-3 is-size-4-mobile has-margin-bottom-2">
@@ -64,7 +31,7 @@ const page = createClass({
   ${entry.getIn(["data", "products"], []).map(pro =>   html`
     <div class="flex-item" >
       <div
-        class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height lazy" data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
+        class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height " data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
         <a class="modal-button" data-target="${pro.get("title")}">
           <div class="custom-card-content">
             <div class="content">
