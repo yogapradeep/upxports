a5b243e6af043b2cd2bcda13f52e0b7c5d969b88
pradeep <v.yogapradeep@gmail.com>
2019-10-14 22:44:03 +0530

    product title

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -28,7 +28,7 @@ const page = createClass({
 
     ${entry.getIn(["data", "products"], []).map(pro =>   html` 
     
-    <p>${product.get("text")}</p>
+    <p>${product.get("title")}</p>
     `
     )
     } 
