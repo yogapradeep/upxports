d28d4e65d68b6876d15a7376417bf2c4529e690b
pradeep <v.yogapradeep@gmail.com>
2019-10-14 18:06:57 +0530

    updated index.js

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -1,6 +1,8 @@
 import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
+
+
 // Preview component for a Post
 const page = createClass({
   render() {
@@ -10,6 +12,37 @@ const page = createClass({
 
     
   <section id="products" class="section is-medium has-css_bg-color">
+  <div class="container">
+  <header class="has-text-centered">
+    <h3 class="title is-3 is-size-4-mobile has-margin-bottom-2">
+      We offer a <span class="has-text-purple">wide range</span> of
+      commodities
+    </h3>
+    <h4 class="subtitle is-4 is-size-5-mobile has-text-grey-light has-desktop-margin-bottom-0 has-margin-bottom-3">
+      From manufacturers valuing quality
+    </h4>
+  </header>
+  
+  <div class="flex-block has-desktop-margin-bottom-1 ">
+  ${entry.getIn(["data", "products"], []).map(pro =>   html`
+    <div class="flex-item" >
+      <div
+        class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height lazy" data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
+        <a class="modal-button" data-target="${pro.get("title")}">
+          <div class="custom-card-content">
+            <div class="content">
+              <p class=" title is-4 has-text-white has-text-centered">
+              ${pro.get("title")}
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+    `)} 
+   </div>
+  </div>
+
   ${entry.getIn(["data", "products"], []).map(pro =>   html`
     <div id="${pro.get("title")}" class="modal">
       <div class="modal-background has-fadein-animation"></div>
@@ -21,10 +54,10 @@ const page = createClass({
         <section class="modal-card-body">
           <div class="content">
             <figure class="image is-4by3 is-hidden-desktop has-no-margin">
-              <img class="lazy" data-src=${getAsset(pro.get("image1"))} alt="${pro.get("title")}" loading="lazy" />
+              <img class="lazy" data-src="${getAsset(pro.get("image1"))}" alt="${pro.get("title")}" loading="lazy" />
             </figure>
             <figure class="image is-16by9 is-hidden-touch has-no-margin">
-              <img class="lazy" data-src=${getAsset(pro.get("image2"))} alt="${pro.get("title")}" loading="lazy" />
+              <img class="lazy" data-src="${getAsset(pro.get("image2"))}" alt="${pro.get("title")}" loading="lazy" />
             </figure>
             <p>
             ${pro.get("text")}
@@ -36,7 +69,7 @@ const page = createClass({
                 <div class="custom-container has-margin-bottom-2">
                   <div>
                     <figure class="image is-64x64">
-                      <img class="lazy" data-src=${getAsset(cat.get("image"))} alt="${cat.get("title")}">
+                      <img class="lazy" data-src="${getAsset(cat.get("image"))}" alt="${cat.get("title")}">
                     </figure>
                   </div>
                   <div>
@@ -53,7 +86,7 @@ const page = createClass({
               <div class="custom-container has-margin-bottom-2 has-custom-width-2">
                 <div>
                   <figure class="image is-64x64">
-                    <img class="lazy" data-src=${getAsset(cat.get("image"))} alt="${cat.get("title")}">
+                    <img class="lazy" data-src="${getAsset(cat.get("image"))}" alt="${cat.get("title")}">
                   </figure>
                 </div>
                 <div>
@@ -76,36 +109,6 @@ const page = createClass({
   `)} 
    
     
-    <div class="container">
-      <header class="has-text-centered">
-        <h3 class="title is-3 is-size-4-mobile has-margin-bottom-2">
-          We offer a <span class="has-text-purple">wide range</span> of
-          commodities
-        </h3>
-        <h4 class="subtitle is-4 is-size-5-mobile has-text-grey-light has-desktop-margin-bottom-0 has-margin-bottom-3">
-          From manufacturers valuing quality
-        </h4>
-      </header>
-      
-      <div class="flex-block has-desktop-margin-bottom-1 ">
-      ${entry.getIn(["data", "products"], []).map(pro =>   html`
-        <div class="flex-item" >
-          <div
-            class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height lazy" data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
-            <a class="modal-button" data-target="${pro.get("title")}">
-              <div class="custom-card-content">
-                <div class="content">
-                  <p class=" title is-4 has-text-white has-text-centered">
-                  ${pro.get("title")}
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        `)} 
-       </div>
-      </div>
     </div>
   </section>
  
@@ -117,3 +120,8 @@ const page = createClass({
     
 }
 });
+
+
+document.addEventListener("DOMContentLoaded",()=>{const e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(e=>{e.addEventListener("click",()=>{const t=e.dataset.target,n=document.getElementById(t);e.classList.toggle("is-active"),n.classList.toggle("is-active")})});var t=document.documentElement,n=document.querySelectorAll(".modal"),o=document.querySelectorAll(".modal-button"),c=document.querySelectorAll(".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot");function d(){t.classList.remove("is-clipped"),n.forEach(function(e){e.classList.remove("is-active")})}o.length>0&&o.forEach(function(e){e.addEventListener("click",function(){!function(e){var n=document.getElementById(e);t.classList.add("is-clipped"),n.classList.add("is-active")}(e.dataset.target)})}),c.length>0&&c.forEach(function(e){e.addEventListener("click",function(){d()})}),document.addEventListener("keydown",function(e){27===(e||window.event).keyCode&&d()}),window.lazyLoadOptions={elements_selector:".lazy"}});
+
+<script async src="assets/js/lazyload.min.js"></script> 
