4df2afc5fb6dd10c01316e6765f67d3eb580d022
pradeep <v.yogapradeep@gmail.com>
2019-10-14 23:30:37 +0530

    script inside the render function

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -2,13 +2,15 @@ import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
 
-document.addEventListener("DOMContentLoaded",()=>{const e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(e=>{e.addEventListener("click",()=>{const t=e.dataset.target,n=document.getElementById(t);e.classList.toggle("is-active"),n.classList.toggle("is-active")})});var t=document.documentElement,n=document.querySelectorAll(".modal"),o=document.querySelectorAll(".modal-button"),c=document.querySelectorAll(".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot");function d(){t.classList.remove("is-clipped"),n.forEach(function(e){e.classList.remove("is-active")})}o.length>0&&o.forEach(function(e){e.addEventListener("click",function(){!function(e){var n=document.getElementById(e);t.classList.add("is-clipped"),n.classList.add("is-active")}(e.dataset.target)})}),c.length>0&&c.forEach(function(e){e.addEventListener("click",function(){d()})}),document.addEventListener("keydown",function(e){27===(e||window.event).keyCode&&d()})});
+
 
 // Preview component for a Post
 const page = createClass({
   render() {
     const entry = this.props.entry;
     const getAsset=this.props.getAsset;
+
+    document.addEventListener("DOMContentLoaded",()=>{const e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(e=>{e.addEventListener("click",()=>{const t=e.dataset.target,n=document.getElementById(t);e.classList.toggle("is-active"),n.classList.toggle("is-active")})});var t=document.documentElement,n=document.querySelectorAll(".modal"),o=document.querySelectorAll(".modal-button"),c=document.querySelectorAll(".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot");function d(){t.classList.remove("is-clipped"),n.forEach(function(e){e.classList.remove("is-active")})}o.length>0&&o.forEach(function(e){e.addEventListener("click",function(){!function(e){var n=document.getElementById(e);t.classList.add("is-clipped"),n.classList.add("is-active")}(e.dataset.target)})}),c.length>0&&c.forEach(function(e){e.addEventListener("click",function(){d()})}),document.addEventListener("keydown",function(e){27===(e||window.event).keyCode&&d()})});
 
     return html`
 
@@ -48,10 +50,10 @@ const page = createClass({
           <section class="modal-card-body">
             <div class="content">
               <figure class="image is-4by3 is-hidden-desktop has-no-margin">
-                <img class="lazy" data-src="${getAsset(pro.get("image1"))}" alt="${pro.get("title")}" loading="lazy" />
+                <img class="lazy" data-src="${getAsset(pro.get("image1"))}" alt="${pro.get("title")}"  />
               </figure>
               <figure class="image is-16by9 is-hidden-touch has-no-margin">
-                <img class="lazy" data-src="${getAsset(pro.get("image2"))}" alt="${pro.get("title")}" loading="lazy" />
+                <img class="lazy" data-src="${getAsset(pro.get("image2"))}" alt="${pro.get("title")}"  />
               </figure>
               <p>
               ${pro.get("text")}
@@ -123,7 +125,7 @@ const page = createClass({
     ${entry.getIn(["data", "products"], []).map(pro =>   html`
       <div class="flex-item" >
         <div
-          class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height lazy" data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
+          class="card has-shadow has-something product-bg  has-text-white has-radius has-equal-height " data-bg="linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${getAsset(pro.get("bgimage"))})">
           <a class="modal-button" data-target="${pro.get("title")}">
             <div class="custom-card-content">
               <div class="content">
