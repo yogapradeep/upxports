b069d8c6e344e48ef8e6702b802ef2f2692f699f
pradeep <v.yogapradeep@gmail.com>
2019-10-14 21:57:34 +0530

    updated index,products

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -13,6 +13,77 @@ const page = createClass({
 
     
   <section id="products" class="section is-medium has-css_bg-color">
+  ${entry.getIn(["data", "products"], []).map(pro =>   html`
+    <div id="${pro.get("title")}" class="modal">
+      <div class="modal-background has-fadein-animation"></div>
+      <div class="modal-card has-fadein-animation">
+        <header class="modal-card-head">
+          <p class="modal-card-title">${pro.get("title")}</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="content">
+            <figure class="image is-4by3 is-hidden-desktop has-no-margin">
+              <img class="lazy" data-src="${getAsset(pro.get("image1"))}" alt="${pro.get("title")}" loading="lazy" />
+            </figure>
+            <figure class="image is-16by9 is-hidden-touch has-no-margin">
+              <img class="lazy" data-src="${getAsset(pro.get("image2"))}" alt="${pro.get("title")}" loading="lazy" />
+            </figure>
+            <p>
+            ${pro.get("text")}
+            </p>
+            <h2 class="has-margin-bottom-2">${pro.get("category")}</h2>
+          
+            <div class="custom-container-2 is-hidden-tablet">
+            ${entry.getIn(["data", "categories" ], []).map(cat =>   html`
+                <div class="custom-container has-margin-bottom-2">
+                  <div>
+                    <figure class="image is-64x64">
+                      <img class="lazy" data-src="${getAsset(cat.get("image"))}" alt="${cat.get("title")}">
+                    </figure>
+                  </div>
+                  <div>
+                    <p class="title is-5">
+                      <strong>${cat.get("title")}</strong>
+                    </p>
+                  </div>
+                </div>
+                `
+                )
+              }
+              
+            </div>
+           <div class="custom-container-2 is-hidden-mobile">
+           ${entry.getIn(["data", "categories" ], []).map(cat =>   html`
+              <div class="custom-container has-margin-bottom-2 has-custom-width-2">
+                <div>
+                  <figure class="image is-64x64">
+                    <img class="lazy" data-src="${getAsset(cat.get("image"))}" alt="${cat.get("title")}">
+                  </figure>
+                </div>
+                <div>
+                  <p class="title is-5">
+                    <strong>${cat.get("title")}</strong>
+                  </p>
+                </div>
+              </div>
+              `
+              )
+            } 
+            </div>
+               
+           <blockquote>
+           ${pro.get("flagship")}
+            </blockquote>   
+           
+        </section>
+      </div>
+    </div>
+   
+  `)} 
+   
+
+
   <div class="container">
   <header class="has-text-centered">
     <h3 class="title is-3 is-size-4-mobile has-margin-bottom-2">
