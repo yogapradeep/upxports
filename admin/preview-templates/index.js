f5951f0f452745ffeae43432fed5b857c40dab00
pradeep <v.yogapradeep@gmail.com>
2019-10-15 16:04:59 +0530

    linked the modal script file in index.html

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -1,21 +1,2 @@
 import page from "/admin/preview-templates/products.js";
 CMS.registerPreviewTemplate("product", page);
-
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-
