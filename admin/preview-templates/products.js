c9584377412eb6e7bff4ad0df033b8305f08fe21
pradeep <v.yogapradeep@gmail.com>
2019-10-16 00:13:16 +0530

    update

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -4,16 +4,16 @@ export default class page extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
 
-    return <div>
+    return ` <div>
     
-    {(entry.getIn(["data", "products"]) || []).map((pro, i) => <div>
+    ${(entry.getIn(["data", "products"]) || []).map((pro, i) => <div>
     <p>{pro.get("title")}</p>
     <img src={getAsset(pro.get("image1"))} alt=""  style={{width: "240px"}}/>
     
   </div>)}
 
     </div>
-   
+   `;
 
   }
 }
