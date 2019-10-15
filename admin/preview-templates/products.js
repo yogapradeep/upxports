a4a6b6a014cea565367404305f84058e6f26c7a0
pradeep <v.yogapradeep@gmail.com>
2019-10-15 15:54:59 +0530

    modal script in index.js

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -1,23 +1,6 @@
 import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
 
 
 // Preview component for a Post
