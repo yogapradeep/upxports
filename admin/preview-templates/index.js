dabd696cf641d292cf0a8fcccdfdedeaff7342f1
pradeep <v.yogapradeep@gmail.com>
2019-10-15 17:00:43 +0530

    modal script inside render

--- admin/preview-templates/index.js
+++ admin/preview-templates/index.js
@@ -1,3 +1,2 @@
 import page from "/admin/preview-templates/products.js";
 CMS.registerPreviewTemplate("product", page);
-<script  src="/admin/preview-templates/script.js"></script>
--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -8,7 +8,25 @@ const page = createClass({
   render() {
     const entry = this.props.entry;
     const getAsset=this.props.getAsset;
-
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+    
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+    
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+    
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+    
+    
     
 
     return html`
