15101469200fba67025c1e23ae5b4a34967a3fb8
pradeep <v.yogapradeep@gmail.com>
2019-10-16 19:39:54 +0530

    display layout variable

--- admin/preview-templates/products.js
+++ admin/preview-templates/products.js
@@ -1,20 +1,10 @@
-import React from "https://unpkg.com/react@16/umd/react.production.min.js";
+import React from "react";
 
 export default class page extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
 
-    return <div>
-    
-    {(entry.getIn(["data", "products"]) || []).map((pro, i) => 
-    <div key={i}>
-    <p>{pro.get("title")}</p>
-    
-    <img src={getAsset(pro.get("image1"))} alt=""  style={{width: "240px"}}/>
-    <p>{pro.get("text")}</p>
-  </div>)}
-
-    </div>
+    return <p>{entry.getIn(["data", "layout"])}</p>
    
 
   }
